512b417517351c13560a5e23717c3749da81be3f
ichyo <ichyo00@gmail.com>
2023-07-07 22:54:15 +0900

    Parse problem

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 for id in {1..45}
 do
-    wget "https://cdn.icfpcontest.com/problems/$id.json" -P ./problems -nc 
+    wget "https://cdn.icfpcontest.com/problems/$id.json" -P ./problems -nc
     # curl "https://api.icfpcontest.com/problem?problem_id=$id" | jq -r .Success > ./problems/$id.json
 done
 
